fe4058adb3844e62f872addb113a06dabd751c1e
Gabor Nemeth <etgabo@gmail.com>
2026-01-27 21:31:35 +0000

    updated validation scripts

--- Validate-JsonContent.ps1
+++ Validate-JsonContent.ps1
@@ -10,6 +10,7 @@ function LogError()
 {
     Param([string]$errorString)
     Write-Error $errorString
+    Exit 1
 }
 function Validate-URL
 {
@@ -44,22 +45,39 @@ function Validate-FileName
     return $false
 
 }
-function Validate-DependencyOrExtension
+
+function Validate-Id
 {
-    Param($DependencyOrExtensionInfo)
-    $Dependency = $ModManifestHashTable[$DependencyOrExtensionInfo.name]
-    if (!$Dependency) {
-        LogError("$($DependencyOrExtensionInfo.name) is not found in Managed Mod List!")
-        return $false
-    }
-    $foundVersion = $ModManifestHashTable[$DependencyOrExtensionInfo.name].artifacts.version -match $DependencyOrExtensionInfo.minimumVersion
-    if (!$foundVersion)
-    {
-        LogError("$($DependencyOrExtensionInfo.name) version $($DependencyOrExtensionInfo.minimumVersion) is not found in Managed Mod List!")
+    Param($id)
+    $check = $ModManifestHashTable["$id"]
+    if (!$check){
         return $false
     }
     return $true
 }
+function Validate-Relation
+{
+    Param(
+        [Object]
+        $RelationObject
+    )
+
+    $idValid = Validate-Id -id $RelationObject.id
+    $versionValid = Validate-Version -versionString $RelationObject.Version
+    if (!$idValid)
+    {
+        LogError("$($RelationObject.id) is invalid!")
+        return $false
+    }
+    if (!$versionValid)
+    {
+        LogError("$($RelationObject.version) is invalid!")
+        return $false
+    }
+
+    return $true
+}
+
 function Validate-Version
 {
     Param(
@@ -93,25 +111,57 @@ function Validate-FileHashFormat
 
 }
 
-$parsedMod = Get-Content $Path | ConvertFrom-Json
-Write-Host "Validating $($parsedMod.name)..."
-$parsedMod
+try
+{
+    $parsedMod = Get-Content $Path | ConvertFrom-Json
+    Write-Host "Validating $($parsedMod.id)..."
+    $parsedMod
 
-Write-Host "Validating infoUrl $($parsedMod.infoURL): $(Validate-URL -URL $parsedMod.infoURL -artifact $false)"
-Write-Host "Validating supportedVersion $($parsedMod.supportedVersion): $(Validate-Version -versionString $parsedMod.supportedVersion)"
-Write-Host "Validating dependencies..."
-foreach ($dependency in $parsedMod.dependencies)
-{
-    Write-Host "Validating dependency $($dependency.name): $(Validate-DependencyOrExtension -DependencyOrExtensionInfo $dependency)"
-}
-Write-Host "Validating artifacts..."
-foreach ($artifact in $parsedMod.artifacts)
-{
-    Write-Host "Validating fileName: $(Validate-FileName -fileName $artifact.fileName)"
-    Write-Host "Validating artifactUrl: $(Validate-URL -URL $artifact.artifactUrl -artifact $true)"
-    Write-Host "Validating fileHash: $(Validate-FileHashFormat -hashString $artifact.hash)"
-    foreach ($extendable in $artifact.extends)
+    Write-Host "Validating infoUrl $($parsedMod.infoURL): $(Validate-URL -URL $parsedMod.infoURL -artifact $false)"
+    Write-Host "Validating dependencies..."
+
+    Write-Host "Validating artifacts..."
+    foreach ($artifact in $parsedMod.artifacts)
     {
-        Write-Host "Validating extends: $(Validate-DependencyOrExtension -DependencyOrExtensionInfo $extendable)"
+        Write-Host "Validating $($parsedMod.id) artifact version $($artifact.version)"
+        $artifact
+        Write-Host "Validating fileName: $(Validate-FileName -fileName $artifact.fileName)"
+        Write-Host "Validating artifactUrl: $(Validate-URL -URL $artifact.downloadUrl -artifact $true)"
+        Write-Host "Validating fileHash: $(Validate-FileHashFormat -hashString $artifact.hash)"
+        Write-Host "Validating version: $(Validate-Version -versionString $artifact.version)"
+        Write-Host "Validating gameVersion: $(Validate-Version -versionString $artifact.gameVersion)"
+        if ($artifact.dependencies)
+        {
+            Write-Host "Validating $($parsedMod.id) artifact version $($artifact.version) DEPENDENCIES:"
+
+            foreach ($dependency in $artifact.dependencies)
+            {
+                $dependency
+                Write-Host "Validating dependency $($dependency.name): $(Validate-Relation -RelationObject $dependency)"
+            }
+        }
+        if ($artifact.incompatibilities)
+        {
+            Write-Host "Validating $($parsedMod.id) artifact version $($artifact.version) INCOMPATIBILITIES:"
+
+            foreach ($incompatibility in $artifact.incompatibilities)
+            {
+                $incompatibility
+                Write-Host "Validating dependency $($incompatibility.name): $(Validate-Relation -RelationObject $incompatibility)"
+            }
+        }
+        if ($artifact.extends)
+        {
+            Write-Host "Validating $($parsedMod.id) artifact version $($artifact.version) EXTENDS:"
+            $artifact.extends
+
+            Write-Host "Validating extends: $(Validate-Relation -RelationObject $artifact.extends)"
+        }
     }
+    Exit 0
 }
+catch
+{
+    Exit 1
+}
+Exit 1
